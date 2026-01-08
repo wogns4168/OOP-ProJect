cdc2314822d66682d467ed876c55baf7ae29d368
Jaehoon Jeong <wogns4168@naver.com>
2026-01-08 17:21:00 +0900

    메뉴리스트 구현

--- MoneyWeapon/Utils/MenuList.cs
+++ MoneyWeapon/Utils/MenuList.cs
@@ -9,5 +9,124 @@ namespace MoneyWeapon.Utils
     internal class MenuList
     {
         private List<(string text, Action action)> _menus;
+        public int CurrentIndex { get; private set; }
+        private Ractangle _outline;
+        private int _maxLength;
+
+
+
+        public MenuList(params (string, Action)[] menuTexts)
+        {
+            if (menuTexts.Length == 0)
+            {
+                _menus = new List<(string, Action)>();
+            }
+            else
+            {
+                _menus = menuTexts.ToList();
+            }
+
+            for (int i = 0; i < _menus.Count; i++)
+            {
+                int textWidth = _menus[i].text.GetTextWidth();
+
+                if (_maxLength < textWidth)
+                {
+                    _maxLength = textWidth;
+                }
+            }
+
+            _outline = new Ractangle(width: _maxLength + 4, height: _menus.Count + 2);
+        }
+
+        public void Reset()
+        {
+            CurrentIndex = 0;
+        }
+
+        public void Select()
+        {
+            if (_menus.Count == 0) return;
+
+            _menus[CurrentIndex].action?.Invoke();
+
+            if(_menus.Count == 0) CurrentIndex = 0;
+            else if (CurrentIndex >= _menus.Count) CurrentIndex = _menus.Count - 1;
+        }
+
+        public void Add(string text, Action action)
+        {
+            _menus.Add((text, action));
+
+            int textWidth = text.GetTextWidth();
+            if (_maxLength < textWidth)
+            {
+                _maxLength = textWidth;
+            }
+
+            _outline.Width = _maxLength + 6;
+            _outline.Height++;
+        }
+
+        public void Remove()
+        {
+            _menus.RemoveAt(CurrentIndex);
+
+            int max = 0;
+
+            foreach ((string text, Action action) in _menus)
+            {
+                int textWidth = text.GetTextWidth();
+
+                if (max < textWidth) max = textWidth;
+
+            }
+
+            if (_maxLength != max) _maxLength = max;
+
+            _outline.Width = _maxLength + 6;
+            _outline.Height--;
+        }
+
+        public void Render(int x, int y)
+        {
+            _outline.X = x;
+            _outline.Y = y;
+            _outline.Draw();
+
+            for (int i = 0; i < _menus.Count; i++)
+            {
+                y++;
+                Console.SetCursorPosition(x + 1, y);
+
+                if (i == CurrentIndex)
+                {
+                    "->".Print(ConsoleColor.Green);
+                    _menus[i].text.Print(ConsoleColor.Green);
+                    continue;
+                }
+                else
+                {
+                    Console.Write("  ");
+                    _menus[i].text.Print();
+                }
+            }
+        }
+
+        public void SelectUp()
+        {
+            CurrentIndex--;
+
+            if (CurrentIndex < 0)
+                CurrentIndex = 0;
+        }
+
+        public void SelectDown()
+        {
+            CurrentIndex++;
+
+            if (CurrentIndex >= _menus.Count)
+                CurrentIndex = _menus.Count - 1;
+        }
     }
 }
--- MoneyWeapon/Utils/TextExtensions.cs
+++ MoneyWeapon/Utils/TextExtensions.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace MoneyWeapon.Utils
 {
-    internal class TextExtensions
+    internal static class TextExtensions
     {
+        public static void Print(this string text, ConsoleColor color = ConsoleColor.Gray)
+        {
+            if (color != ConsoleColor.Gray) Console.ForegroundColor = color;
+
+            Console.Write(text);
+
+            if (color != ConsoleColor.Gray) Console.ResetColor();
+        }
+
+        public static void Print(this char character, ConsoleColor color = ConsoleColor.Gray)
+        {
+            if (color != ConsoleColor.Gray) Console.ForegroundColor = color;
+
+            Console.Write(character);
+
+            if (color != ConsoleColor.Gray) Console.ResetColor();
+        }
+
+        public static int GetTextWidth(this string text)
+        {
+            int width = 0;
+            foreach (char c in text)
+            {
+                width += c.GetCharacterWidth();
+            }
+            return width;
+        }
+
+        public static int GetCharacterWidth(this char character)
+        {
+            // 한글 음절(가-힣), CJK 호환문자, 전각 기호/문자 범위는 2칸으로 처리
+            if ((character >= '\uAC00' && character <= '\uD7A3') || // 한글 완성형
+                (character >= '\u1100' && character <= '\u11FF') || // 한글 자모
+                (character >= '\u3130' && character <= '\u318F') || // 한글 호환 자모
+                (character >= '\uFF01' && character <= '\uFF60') || // 전각 기호/영숫자
+                (character >= '\uFFE0' && character <= '\uFFE6'))   // 전각 특수기호
+            {
+                return 2;
+            }
+            else
+            {
+                return 1;
+            }
+        }
     }
 }
