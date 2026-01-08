92abff8b4bf234f80851d5586674cb9f671cc262
Jaehoon Jeong <wogns4168@naver.com>
2026-01-08 17:27:15 +0900

    타이틀씬 (생성자, 렌더, 게임종료) 구현

--- MoneyWeapon/Scenes/TitleScene.cs
+++ MoneyWeapon/Scenes/TitleScene.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MoneyWeapon.Managers;
+using MoneyWeapon.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,20 @@ namespace MoneyWeapon.Scenes
 {
     internal class TitleScene : Scene
     {
+        private MenuList _titleMenu;
+
+        public TitleScene()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            _titleMenu = new MenuList();
+            _titleMenu.Add("게임 시작", GameStart);
+            _titleMenu.Add("크레딧", Credit);
+            _titleMenu.Add("게임 종료", GameQuit);
+        }
         public override void Enter()
         {
         }
@@ -18,10 +34,29 @@ namespace MoneyWeapon.Scenes
 
         public override void Render()
         {
+            Console.SetCursorPosition(5, 1);
+            GameManager.GameName.Print(ConsoleColor.Yellow);
+
+            _titleMenu.Render(8, 5);
         }
 
         public override void Update()
         {
+        }
+
+        public void GameStart()
+        {
+
+        }
+
+        public void Credit()
+        {
+
+        }
+
+        public void GameQuit()
+        {
+            GameManager.IsGameOver = true;
         }
     }
 }
