11191b73980e3c649b73265470b03e09f05870c0
Jaehoon Jeong <wogns4168@naver.com>
2026-01-08 16:49:16 +0900

    씬 생성 및 구현

---
+++ MoneyWeapon/Scenes/Scene.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWeapon.Scenes
+{
+    internal abstract class Scene
+    {
+        public abstract void Enter();
+        public abstract void Update();
+        public abstract void Render();
+        public abstract void Exit();
+    }
+}
