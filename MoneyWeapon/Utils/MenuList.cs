63d1078d98c09ea4c00f97b37f17d040c50b6784
Jaehoon Jeong <wogns4168@naver.com>
2026-01-08 16:52:06 +0900

    메뉴 리스트 생성

---
+++ MoneyWeapon/Utils/MenuList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWeapon.Utils
+{
+    internal class MenuList
+    {
+        private List<(string text, Action action)> _menus;
+    }
+}
