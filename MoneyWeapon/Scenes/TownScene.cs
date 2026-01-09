f7daced8ebf9e3e665ba0b11d0ee307169f2c134
Jaehoon Jeong <wogns4168@naver.com>
2026-01-09 12:41:33 +0900

    구현할 씬들 생성

---
+++ MoneyWeapon/Scenes/TownScene.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWeapon.Scenes
+{
+    internal class TownScene : Scene
+    {
+        public override void Enter()
+        {
+        }
+
+        public override void Exit()
+        {
+        }
+
+        public override void Render()
+        {
+        }
+
+        public override void Update()
+        {
+        }
+    }
+}
