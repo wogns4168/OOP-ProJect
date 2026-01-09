f67db93daabb9f4e3f3aa19cf9cfa9fd3beeb72c
Jaehoon Jeong <wogns4168@naver.com>
2026-01-09 17:09:39 +0900

    플레이어 (이동만) 구현

---
+++ MoneyWeapon/GameObjects/Player.cs
@@ -0,0 +1,62 @@
+﻿using MoneyWeapon.Managers;
+using MoneyWeapon.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWeapon.GameObjects
+{
+    internal class Player : GameObject
+    {
+        public Tile[,] Field { get; set; }
+
+        public Player() 
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            Symbol = 'P';
+        }
+
+        public void Update()
+        {
+            if (InputManager.GetKey(ConsoleKey.UpArrow))
+            {
+                Move(Vector.Up);
+            }
+
+            if (InputManager.GetKey(ConsoleKey.DownArrow))
+            {
+                Move(Vector.Down);
+            }
+
+            if (InputManager.GetKey(ConsoleKey.LeftArrow))
+            {
+                Move(Vector.Left);
+            }
+
+            if (InputManager.GetKey(ConsoleKey.RightArrow))
+            {
+                Move(Vector.Right);
+            }
+        }
+
+        public void Move(Vector direction)
+        {
+            Vector Current = Position;
+            Vector nextPos = Position + direction;
+
+            GameObject nextTileObject = Field[nextPos.Y, nextPos.X].OnTileObject;
+
+            if (nextTileObject != null) return;
+
+            Field[Position.Y, Position.X].OnTileObject = null;
+            Field[nextPos.Y, nextPos.X].OnTileObject = this;
+            Position = nextPos;
+        }
+    }
+}
---
+++ MoneyWeapon/Utils/Tile.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MoneyWeapon.GameObjects;
+
+namespace MoneyWeapon.Utils
+{
+    internal struct Tile
+    {
+        public GameObject OnTileObject { get; set; }
+        public Vector Position { get; set; }
+
+        public bool HasGameObject => OnTileObject != null;
+
+        public Tile (Vector position)
+        {
+            Position = position;
+            OnTileObject = null;
+        }
+
+        public void Print()
+        {
+            if(HasGameObject)
+            {
+                OnTileObject.Symbol.Print();
+            }
+            else
+            {
+                ' '.Print();
+            }
+        }
+    }
+}
