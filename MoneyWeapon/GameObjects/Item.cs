e998f4b3707897ef2171f0913df01338444fafd1
Jaehoon Jeong <wogns4168@naver.com>
2026-01-11 16:01:02 +0900

    아이템 인터페이스 구현

---
+++ MoneyWeapon/GameObjects/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyWeapon.GameObjects
+{
+    internal interface Item
+    {
+        string Name { get; }
+        int Price { get; }
+
+        int MinPrice { get; }
+        int MaxPrice { get; }
+
+        int MaxQuantity { get; }
+    }
+}
